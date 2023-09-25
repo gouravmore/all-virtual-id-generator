cce01fed02279709fa0c96b18615c6d40f90b4f5
Gourav More <gourav_m@tekditechnologies.com>
2023-09-25 17:20:56 +0530

    all-virtual id generator

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const crypto = require('crypto');
+const { MongoClient } = require('mongodb');
+require('dotenv').config(); // Load environment variables from .env file
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Function to generate a random 10-digit number
+function generateRandomID() {
+  return Math.floor(1000000000 + Math.random() * 9000000000);
+}
+
+// Define a route to generate a virtual ID for a user
+app.get('/generateVirtualID', async (req, res) => {
+  const username = req.query.username;
+  const password = req.query.password;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+
+  // Connect to MongoDB using the environment variables
+  const client = new MongoClient(process.env.MONGODB_URL);
+
+  try {
+    await client.connect();
+    const db = client.db(process.env.DB_NAME);
+
+    // Check if the user exists in the database
+    const usersCollection = db.collection('allvuser');
+    const user = await usersCollection.findOne({ username });
+
+    if (user) {
+      // User already exists, return their virtual ID
+      return res.json({ virtualID: user.virtualID });
+    } else {
+      // User does not exist, generate a new virtual ID and store it in the database
+      const virtualID = generateRandomID();
+      await usersCollection.insertOne({ username, password, virtualID });
+      res.json({ virtualID });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database error' });
+  } finally {
+    client.close();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
